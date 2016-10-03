14556d2ac931e8f8279714f53aa53600a048f84f
[cknovy] <[cknovy@gmail.com]>
2016-10-03 12:27:02 -0400

    Fixed output so it wasn't so heckin big.

--- run_Analysis.R
+++ run_Analysis.R
@@ -1,10 +1,11 @@
 library(reshape2)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Load activity tables
-features <- read.table("../Desktop/Classes/R/UCI HAR Dataset/features.txt")
+features <- read.table("UCI HAR Dataset/features.txt")
 features[,2] <- as.character(features[,2])
 
-aLabs<- read.table("../Desktop/Classes/R/UCI HAR Dataset/activity_labels.txt")
+aLabs<- read.table("UCI HAR Dataset/activity_labels.txt")
 aLabs[,2] <- as.character(aLabs[,2])
 
 
@@ -18,14 +19,14 @@ feat.n = gsub('-std', 'Std', feat.n)
 feat.n <- gsub('[-()]', '', feat.n)
 
 
-train <- read.table("../Desktop/Classes/R/UCI HAR Dataset/train/X_train.txt")[feat]
-act.train <- read.table("../Desktop/Classes/R/UCI HAR Dataset/train/Y_train.txt")
-sub.train <- read.table("../Desktop/Classes/R/UCI HAR Dataset/train/subject_train.txt")
+train <- read.table("UCI HAR Dataset/train/X_train.txt")[feat]
+act.train <- read.table("UCI HAR Dataset/train/Y_train.txt")
+sub.train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 train <- cbind(sub.train, act.train, train)
 
-test <- read.table("../Desktop/Classes/R/UCI HAR Dataset/test/X_test.txt")[feat]
-act.test <- read.table("../Desktop/Classes/R/UCI HAR Dataset/test/Y_test.txt")
-sub.test <- read.table("../Desktop/Classes/R/UCI HAR Dataset/test/subject_test.txt")
+test <- read.table("UCI HAR Dataset/test/X_test.txt")[feat]
+act.test <- read.table("UCI HAR Dataset/test/Y_test.txt")
+sub.test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 test <- cbind(sub.test, act.test, test)
 
 # Step 4: merge datasets and add labels
@@ -40,6 +41,6 @@ all.m <- melt(all, id = c("subject", "activity"))
 all.mean <- dcast(all.m, subject + activity ~ variable, mean)
 
 #Step 5: Creating tidy data set
-write.table(all.m, "../Desktop/Classes/R/itssotidy.txt", row.name = F)
+write.table(all.mean, "itssotidy.txt", row.name = F)
 
 
